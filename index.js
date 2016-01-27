8a5e605f9341fa609797e442b4bca6e76f23da16
aronysidoro <pyaaron@gmail.com>
2016-01-27 06:43:27 -0800

    first commit - everything up until 'fetching from the server'

---
+++ index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { render } from 'react-dom';
+import marked from 'marked';
+
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var Comment = React.createClass({
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commenForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.props.data}/>
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+render(
+    <CommentBox data={data} />,
+    document.getElementById('container')
+);
